982796be608ca6cc041a77db912c001c3196a038
Tiago Iwamoto <tiago.iwamoto@gmail.com>
2018-08-29 08:21:19 -0300

    Mongo connection

--- index.php
+++ index.php
@@ -25,35 +25,58 @@ posso chamar paginas, controllers
 ini_set('display_errors', true);
 
 
-$app->post('/mongo', function (){
-
-//    $pdo = new PDO('mongodb://localhost:27017');
+//$app->post('/mongo', function (){
 //
-//    print_r($pdo);
+////    $pdo = new PDO('mongodb://localhost:27017');
+////
+////    print_r($pdo);
+//
+//    $inputJSON = file_get_contents('php://input');
+//    $input= json_decode( $inputJSON );
+//    print_r($input);
+//    return '';
+//});
 
-    $inputJSON = file_get_contents('php://input');
-    $input= json_decode( $inputJSON );
-    print_r($input);
-    return '';
-});
-
-$app->get('/mongo/teste', function (){
+$app->post('/mongo/teste', function (){
 
     try{
-        //$mongo = new MongoDB\Client("mongodb://127.0.0.1:27017");
-        //$mongo = new \MongoDB\Client("mongodb://localhost:27017/efika");
-        //$mongo = new Mongo("mongodb://localhost:27017/efika");
-        $mongo = new Mongo('mongodb://localhost:27017/efika');
+        $mongo = new MongoDB\Client("mongodb://127.0.0.1:27017");
         $db = $mongo->efika;
-        $col = $db->users;
-        $total = $col->count(true);
-        echo ($total) ." registros encontrados.<p>";
-        $mongo->close();
-        return '';
+
+        $filter = array('isUpdated'=>false);
+//        foreach ($db->selectCollection('users')->find($filter) as $json){
+//            echo '<pre>';
+//            print_r($json);
+//            echo '</pre>';
+//        }
+        $users = $db->selectCollection('users')->find($filter);
+        $res = json_encode(Array(
+            'code' => 200,
+            'msg' => 'Registros recuperados com sucesso !',
+            'users' => $users->toArray()
+        ));
+        return new \Symfony\Component\HttpFoundation\Response(
+            $res,
+            200,
+            array('Content-Type' => 'application/json'));
     }catch (\Exception $e){
         print_r($e->getMessage());
-        return '';
+        return Array(
+            'code' => 500,
+            'msg' => $e->getMessage()
+        );
     }
+});
+
+$app->post('/efika/users', function (){
+    $inputJSON = file_get_contents('php://input');
+    $input= json_decode( $inputJSON );
+    //print_r($input);
+    return json_encode(Array(
+        'code' => 200,
+        'msg' => 'Usuarios sincronizados com sucesso !',
+        'users' => $input
+    ));
 });
 
 $app->get('/', function (){
--- servicos.php
+++ servicos.php
@@ -8,7 +8,7 @@
 
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
-$app->register(new \App\sync\util\PdoServiceProvider());
+//$app->register(new \App\sync\util\PdoServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__ . '/src/view',
     'twig.options' => [
@@ -16,10 +16,10 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
     ]
 ]);
 
-$app['me'] = function ($c){
-    return new \App\sync\dao\CustomerDao($c['pdo']);
-};
-
-$app['me.controller'] = function ($c){
-    return new \App\sync\controller\MeController($c['me']);
-};
+//$app['me'] = function ($c){
+//    return new \App\sync\dao\CustomerDao($c['pdo']);
+//};
+//
+//$app['me.controller'] = function ($c){
+//    return new \App\sync\controller\MeController($c['me']);
+//};
